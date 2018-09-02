16d76bee8445553e5edbc11d946ce98117928dea
Nikhita <nikhitazulka@gmail.com>
2018-09-02 10:15:10 +0530

    External script file added

---
+++ script.js
@@ -0,0 +1,33 @@
+function myFunction() {
+		    var x = document.getElementById("snackbar");
+		    x.className = "show";
+		    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+		}
+
+		
+		$('document').ready(function(){
+
+		  // Add smooth scrolling to all links
+		  $(".links").on('click', function(event) {
+		  	//console.log("Clicked the link");
+
+		    // Make sure this.hash has a value before overriding default behavior
+		    if (this.hash !== "") {
+		      // Prevent default anchor click behavior
+		      event.preventDefault();
+
+		      // Store hash
+		      var hash = this.hash;
+
+		      // Using jQuery's animate() method to add smooth page scroll
+		      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+		      $('html, body').animate({
+		        scrollTop: $(hash).offset().top
+		      }, 800, function(){
+		   
+		        // Add hash (#) to URL when done scrolling (default click behavior)
+		        window.location.hash = hash;
+		      });
+		    } // End if
+		  });
+		});
